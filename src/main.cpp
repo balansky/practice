da2dcddb87672e26a8861e911c82bca677cb0e52
andy <wudi198@gmail.com>
2019-01-03 12:15:29 -0800

    change structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include"Student.h"
 
+using namespace std;
+
 int main(int argc, char *argv[]){
     Student s("Joe");
     s.display();
+    // int *pf;
+    // pf = new int [25];
+
+//    Student s("Joe");
+//    s.display();
     return 0;
 }
